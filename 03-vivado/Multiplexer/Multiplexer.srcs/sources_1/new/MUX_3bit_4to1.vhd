008cae9f71cbb5320d88247a385d41752015c7bc
Lukáš Lipenský <240652@vutbr.cz>
2023-02-22 10:52:54 +0100

    nic

---
+++ 03-vivado/Multiplexer/Multiplexer.srcs/sources_1/new/MUX_3bit_4to1.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/22/2023 10:41:12 AM
+-- Design Name: 
+-- Module Name: MUX_3bit_4to1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MUX_3bit_4to1 is
+--  Port ( );
+end MUX_3bit_4to1;
+
+architecture Behavioral of MUX_3bit_4to1 is
+
+begin
+with addr_i select
+    y_o <= a_i when "000",  -- If addr_i = "000" then y_o = a_i
+           b_i when "001",
+           c_i when "010",
+           d_i when others; -- All other combinations
+
+end Behavioral;
